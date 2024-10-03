6e65de2e213e49c8ac350168865625df80b81af7
Fruvvien <hunfantasztik@gmail.com>
2024-10-03 18:30:47 +0200

    asd

--- main.js
+++ main.js
@@ -1,6 +1,52 @@
 import './style.css'
 import 'bootstrap/dist/css/bootstrap.css'
 
+<<<<<<< HEAD
+
+=======
+document.addEventListener("DOMContentLoaded", ()=>{
+>>>>>>> 78c4977 (asd)
 
 
+    const tbody = document.getElementById("tbody");
+    getJson();
+    
+    function getJson(){
+        fetch("")
+        .then((response) => response.json())
+        .then((json) => tbodyCreate(json));
+    }
+
+
+    function tbodyCreate(elements){
+        let element = "";
+
+        elements.forEach(item => {
+            let items = `
+            
+             <tr >
+              <td class="text-start">
+                <button><img src="${}"> </button>
+              </td>
+              <td class="text-start">
+                 <button><img src="${}"> </button>
+              </td>
+              <td class="text-end">
+                ${}
+              </td>
+              <td class="text-center">
+                <button>X</button>
+              </td>
+            </tr>
+            `
+
+            element += item;
+        });
+
+        tbody.innerHTML += element;
+        
+    }
+
+
+})
 
