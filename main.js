3e07ec1313cce08d89e052965c395ed209892b1c
Petrik Diak <diak@petrik.hu>
2024-10-03 18:16:48 +0200

    all images added

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
 import './style.css'
 import 'bootstrap/dist/css/bootstrap.css'
 
-//elsőkerék meghajtás vagy össz
-//ülés típus - szövet/bőr
-//sötétített ablak vagy nem
-//dísztárcsa 
-//spoiler 
-//tank mérete
-//szélterelők
