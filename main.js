136b0b970948d2aa7a2c505b064368f13730a83b
Petrik Diak <diak@petrik.hu>
2024-10-04 10:35:54 +0200

    main.js mod

--- main.js
+++ main.js
@@ -7,41 +7,41 @@ document.addEventListener("DOMContentLoaded", ()=>{
     const tbody = document.getElementById("tbody");
     getJson();
     
-    function getJson(){
-        fetch("")
+    async function getJson(){
+        await fetch('./parts.json')
         .then((response) => response.json())
         .then((json) => tbodyCreate(json));
     }
 
 
-    // function tbodyCreate(elements){
-    //     let element = "";
+    function tbodyCreate(elements){
+        let element = "";
 
-    //     elements.forEach(item => {
-    //         let items = `
-            
-    //          <tr >
-    //           <td class="text-start">
-    //             <button><img src="${}"> </button>
-    //           </td>
-    //           <td class="text-start">
-    //              <button><img src="${}"> </button>
-    //           </td>
-    //           <td class="text-end">
-    //             ${}
-    //           </td>
-    //           <td class="text-center">
-    //             <button>X</button>
-    //           </td>
-    //         </tr>
-    //         `
+        for (let i = 0; i < elements.length; i += 2) {
+          let items = `
+            <tr>
+              <td>
+                <button><img src="${'./subaru_kepek/' + elements[i].imgName}"></button>
+              </td>
+              <td>
+                 <button><img src="${'./subaru_kepek/' + elements[i + 1].imgName}"> </button>
+              </td>
+              <td id="price">
+                0
+              </td>
+              <td>
+                <button>X</button>
+              </td>
+            </tr>
+          `
 
-    //         element += item;
-    //     });
+          element += items;
+          
+        }
 
-    //     tbody.innerHTML += element;
+        tbody.innerHTML += element;
         
-    // }
+    }
 
 
 })
